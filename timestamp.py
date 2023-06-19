50481737982f4e6895b4f6eaf0af66f720cef7b8
Alex Vanderhoeff <74937141+avand56@users.noreply.github.com>
2023-06-18 21:24:39 -0400

    Update timestamp.py

--- timestamp.py
+++ timestamp.py
@@ -4,9 +4,12 @@ import time
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/timestamp")
 def time():
 
   # ts stores the time in seconds
   ts = time.time()
   return ts
+
+if __name__ == '__main__':
+    app.run()
